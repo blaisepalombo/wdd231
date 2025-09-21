82242c4660d4fc93f92a0b53a92f30836d5a6251
blaisepalombo <pal19018@byui.edu>
2025-09-20 22:00:25 -0600

    the assignment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
 import { getParkData } from "./parkService.mjs";
 
-const parkData = getParkData();
+const park = getParkData();
+
+// disclaimer link
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = park.url;
+disclaimer.textContent = park.fullName;
+
+// page title
+document.title = park.fullName;
+
+// hero image
+const heroImg = document.querySelector(".hero-banner img");
+heroImg.src = park.images[0].url;
+heroImg.alt = park.images[0].altText;
+
+// hero info
+function heroTemplate(p) {
+  return `
+    <a href="#" class="hero-banner__title">${p.fullName}</a>
+    <p class="hero-banner__subtitle">
+      <span>${p.designation}</span>
+      <span>${p.states}</span>
+    </p>
+  `;
+}
+
+const heroContent = document.querySelector(".hero-banner__content");
+heroContent.innerHTML = heroTemplate(park);
