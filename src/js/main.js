ada68da421bc61b395f2d143ad501403fc725fd7
blaisepalombo <pal19018@byui.edu>
2025-09-25 11:02:33 -0600

    updates

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,16 @@ import { getParkData } from "./parkService.mjs";
 
 const park = getParkData();
 
-// disclaimer link
 const disclaimer = document.querySelector(".disclaimer > a");
 disclaimer.href = park.url;
 disclaimer.textContent = park.fullName;
 
-// page title
 document.title = park.fullName;
 
-// hero image
 const heroImg = document.querySelector(".hero-banner img");
 heroImg.src = park.images[0].url;
 heroImg.alt = park.images[0].altText;
 
-// hero info
 function heroTemplate(p) {
   return `
     <a href="#" class="hero-banner__title">${p.fullName}</a>
@@ -28,3 +24,4 @@ function heroTemplate(p) {
 
 const heroContent = document.querySelector(".hero-banner__content");
 heroContent.innerHTML = heroTemplate(park);
+
