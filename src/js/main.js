4ef0a8e2a28b68c24b2929488ee1dfb78f781f50
blaisepalombo <pal19018@byui.edu>
2025-11-01 16:42:19 -0600

    week 7

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,49 @@
-import "../css/style.css"; // we can do this because we are using Vite...
-import "../css/home.css";
-import { getParkData, getInfoLinks } from "./parkService.mjs";
-import setHeaderFooter from "./setHeaderFooter.mjs";
-import { mediaCardTemplate } from "./templates.mjs";
+import "../css/style.css"
+import "../css/home.css"
+import { getParkData, getInfoLinks } from "./parkService.mjs"
+import setHeaderFooter from "./setHeaderFooter.mjs"
+import { mediaCardTemplate } from "./templates.mjs"
 
 function setParkIntro(data) {
-  const introEl = document.querySelector(".intro");
+  const introEl = document.querySelector(".intro")
   introEl.innerHTML = `<h1>${data.fullName}</h1>
-  <p>${data.description}</p>`;
+  <p>${data.description}</p>`
 }
 
 function setParkInfoLinks(data) {
-  const infoEl = document.querySelector(".info");
-  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
-  const html = data.map(mediaCardTemplate);
-  // join the array of strings into one string and insert it into the section
-  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+  const infoEl = document.querySelector(".info")
+  const html = data.map(mediaCardTemplate)
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""))
 }
 
 async function init() {
-  const parkData = await getParkData();
-  const links = getInfoLinks(parkData.images);
-  setHeaderFooter(parkData);
-  setParkIntro(parkData);
-  setParkInfoLinks(links);
+  const parkData = await getParkData()
+  const links = getInfoLinks(parkData.images)
+  setHeaderFooter(parkData)
+  setParkIntro(parkData)
+  setParkInfoLinks(links)
 }
 
-init();
+function enableNavigation() {
+  const menuButton = document.querySelector("#global-nav-toggle")
+
+  menuButton.addEventListener("click", (ev) => {
+    let target = ev.target
+    document.querySelector(".global-nav").classList.toggle("show")
+
+    if (target.tagName !== "BUTTON") {
+      target = target.closest("button")
+    }
+
+    if (document.querySelector(".global-nav").classList.contains("show")) {
+      target.setAttribute("aria-expanded", true)
+    } else {
+      target.setAttribute("aria-expanded", false)
+    }
+
+    console.log("toggle")
+  })
+}
+
+init()
+enableNavigation()
